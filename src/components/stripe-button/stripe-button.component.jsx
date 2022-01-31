e2b78703315b4278c61f067c4311fe3cd2822650
Praneetha C K <praneetha.ck@publicissapient.com>
2022-01-31 17:35:50 +0530

    Update stripe integration and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+
+  const publishableKey =
+    "pk_test_51KNyNzSGd0xjKsY8cSREb0Kqp0MHz4Iwd33fHt8qaEhwwGeA64vtTXqDrIX14EUMKIKKBidvH1KmlEDTk5rIoktg00KnMrrmx7";
+
+  const onToken = (token) => {
+    console.log(token);
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
