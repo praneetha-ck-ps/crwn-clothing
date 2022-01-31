d184d803c8ff7cc4fd920a4c30c817bca4426dc5
Praneetha C K <praneetha.ck@publicissapient.com>
2022-01-31 11:20:18 +0530

    Updated shop data with new data. Updated cartUtils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,9 +6,9 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, idx) => idx < 4)
+        .filter((_, idx) => idx < 4)
         .map((item) => (
-          <CollectionItem key={item.id} {...item} />
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
